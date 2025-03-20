e26b9818ad6008b0a90e86713901b5eb371238bb
Le Trung Son <sonmartin2003@gmail.com>
2025-03-20 16:45:25 +0700

    built search-result dropdown by using Tippyjs/react

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/6d69637b361e7910a18a7f6126293e9b~tplv-tiktokx-cropcenter:300:300.webp?dr=14577&refresh_token=bc1c541f&x-expires=1742634000&x-signature=wnJhflEs8Ol28HD8iNESkka3Xpw%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=c1333099&idc=my"
+                alt="Hoaa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
