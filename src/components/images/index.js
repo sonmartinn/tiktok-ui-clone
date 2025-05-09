e81745253d7f0687c4e1bbc25f2a61b054e6b88e
Le Trung Son <sonmartin2003@gmail.com>
2025-05-09 14:09:25 +0700

    Applied PropTypes to project

--- src/components/images/index.js
+++ src/components/images/index.js
@@ -1,9 +1,13 @@
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { useState, forwardRef } from 'react';
+import { useState } from 'react';
 import images from '~/assets/images';
 import styles from './image.module.scss';
-
-const Image = forwardRef(({ src, alt, className, fallback: customFallback = images.noImage, ...props }, ref) => {
+// Accessing element.ref was removed in React 19. ref is now a regular prop.
+// It will be removed from the JSX Element type in a future release.
+// So no need to use forwardRef anymore
+// Source: https://react.dev/blog/2024/12/05/react-19#improvements-in-react-19
+const Image = ({ src, alt, className, fallback: customFallback = images.noImage, ref, ...props }) => {
     const [fallback, setFallback] = useState('');
 
     const handleError = () => {
@@ -20,6 +24,13 @@ const Image = forwardRef(({ src, alt, className, fallback: customFallback = imag
             onError={handleError}
         />
     );
-});
+};
+
+Image.propTypes = {
+    src: PropTypes.string,
+    alt: PropTypes.string,
+    className: PropTypes.string,
+    fallback: PropTypes.string,
+};
 
 export default Image;
